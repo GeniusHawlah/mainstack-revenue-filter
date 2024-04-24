93fd49b7b4f2e93e1a4e0e706fea70740ecdd83e
GeniusHawlah <geniusdecode@gmail.com>
2024-04-24 19:23:25 +0100

    Amended some stylings

--- app/revenue/page.js
+++ app/revenue/page.js
@@ -14,7 +14,7 @@ export default async function Revenue({ searchParams }) {
       <div className="630:flex justify-between  gap-x-8 !overflow-y-hidden`">
         {/* //>Chart and balance */}
         <div className="630:w-[65%]">
-          <div className="mb-2 flex items-center justify-between sm:justify-center  gap-x-16">
+          <div className="mb-2 flex items-center justify-between sm:justify-start  gap-x-16">
             <AvailableBalance />
             <button className="px-4 lg:px-7 py-2 lg:py-[14px]  bg-pry-color text-white divide-red-300 hover:bg-opacity-80 rounded-full text-sm 630:text-base font-medium 630:font-semibold">
               Withdraw
